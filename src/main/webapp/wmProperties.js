2840f52a7ff0e0792ef810326260aeb20811ef60
root <root@61949b3d2f95>
2017-07-06 18:10:22 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_WcNriJLpJD",
+  "homePage" : "Main",
+  "name" : "Auto_WcNriJLpJD",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
